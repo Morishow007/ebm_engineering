7d2ef6b27c7e896df19d1b2b44fedfa5907f1e10
DanielSR14 <ydanielschwanck@gmail.com>
2023-10-04 21:14:22 -0300

    Add navigation bar to website
    This change introduces a navigation bar (Navbar) to the website.
    A folder called "Styles" was also created to store the
    CSS files related to the website, including a CSS file
    dedicated to the Navbar component.
    
    Navbar's current CSS is incomplete and needs to be refactored
    for a more efficient organization and to achieve a
    design more consistent with the rest of the site.
    
    An image folder has also been created to store the resources
    visuals needed for the website.

--- src/components/header/NavBar.tsx
+++ src/components/header/NavBar.tsx
@@ -1,11 +1,36 @@
 import { Link } from 'react-router-dom'
+import logo from '../../Images/Navbar/EBM_logo.png'
+import '../../Styles/Navbar.css'
+import { ROUTES } from '../../constants/routes'
 
 export const NavBar = () => {
   return (
-    <>
-      <h1>Navbar</h1>
-      <Link to={'/'}>Home</Link>
-      <Link to={'/About'}>About</Link>
-    </>
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <Link to={ROUTES.landingPage.path}>
+          {/* Add the real logo when we have */}
+          <img src={logo} alt="Logo" />
+        </Link>
+      </div>
+      <ul className="navbar-menu">
+        <li>
+          <Link to={ROUTES.landingPage.path}>Home</Link>
+        </li>
+        <li>
+          <Link to={ROUTES.about.path}>About</Link>
+        </li>
+        <li>
+          {/* Change routes to the correct pages */}
+          <Link to="/contact">Services</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+      <div className="navbar-search">
+        <input type="text" placeholder="Search" />
+        <button type="button">Search</button>
+      </div>
+    </nav>
   )
 }
